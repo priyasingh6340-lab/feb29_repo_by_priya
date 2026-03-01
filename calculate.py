df7207ea614bd6e5e3d01752f02e3fcd77419dfa
priya <priyasingh6340@gmail.com>
2026-03-01 13:11:53 +0530

    local changes

---
+++ calculate.py
@@ -0,0 +1,13 @@
+a = 28
+b = 78
+c = (a+b)
+d = (a-b)
+e = (a*b)
+f = (a/b)
+print(a)
+print(b)      )
+print(c)
+print(d)
+
+print(b)
+print(e)
---
+++ disk-usage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# working on disk usage script
+ echo "enter on disk usage command"
+read p
+echo $p
+echo $p /root
+echo $ /tmp
